1e261f7db8b3109737d03db53ce29422ff878f9c
ZacharyYongBB <zacharyyong_1997@outlook.com>
2022-06-15 12:10:03 +0800

    add stack for 2 screens

--- App.js
+++ App.js
@@ -1,20 +1,38 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+function HomeScreen() {
+ return (
+   <View style={styles.container}>
+     <Text>Open up App.js to start working on your app!</Text>
+     <StatusBar style="auto" />
+   </View>
+ );
+}
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+ return (
+   <NavigationContainer>
+     <Stack.Navigator>
+       <Stack.Screen name="Login" component={LoginScreen} />
+       <Stack.Screen name="Chat" component={ChatScreen} />
+     </Stack.Navigator>
+   </NavigationContainer>
+ );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ container: {
+   flex: 1,
+   backgroundColor: "#fff",
+   alignItems: "center",
+   justifyContent: "center",
+ },
 });
+
+
